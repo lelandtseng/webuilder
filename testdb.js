fef10c3840af317905068f0e61fff5b30270418d
unknown <Administrator@.(none)>
2010-12-29 18:07:19 +0800

    update product module

--- testdb.js
+++ testdb.js
@@ -1,11 +1,8 @@
 require('../lib');
 
 var Db = require('mongodb').Db, Connection = require('mongodb').Connection, Server = require('mongodb').Server, BSON = require('mongodb').BSONNative;
-=======
 
 var Db = require('mongodb').Db, Connection = require('mongodb').Connection, Server = require('mongodb').Server, BSON = require('mongodb').BSONPure;
-
-
 var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : '127.0.0.1';
 var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
 
