9689dc8ae2b4f2ed55763f1ff4af8644dcc345b6
lelandtseng <lelandtseng@lelandtseng-Unknow.(none)>
2011-01-07 21:56:01 +0800

    my

--- modules/admin/index.js
+++ modules/admin/index.js
@@ -1,8 +1,18 @@
+
+/**
+总后台
+    登录
+    用户管理 CURD
+    用户登录
+*/
+
+var Model = require('model').Model;
 var express = require('express');
 var app = module.exports = express.createServer();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.register('.html', require('ejs'));
+var User = new Model('users');
 
 // 验证是否有adm
 function yz(req, res, next){
@@ -21,50 +31,34 @@ app.get('/open_add_page', yz, function(req, res){
 
 // 添加功能
 app.post('/add', yz, function(req, res){
-    db.createCollection('users', function(err, con){
-        con.insert(req.body, function(err){
-            res.redirect('/admin')
-        });
+    User.save(req.body,function(){
+        res.redirect('/admin');
     })
 });
 
-// 全部用户
-function alluser(req, res, next){
-    db.collection('users', function(err, con){
-        con.find({}, function(err, users){
-            console.log(users);
-            var userlist = [];
-            users.toArray(function(err, users){
-                req.users = users;
-                next();
-            });
-            
-        });
-    })
-}
 
 // 显示主页
-app.get('/', yz, alluser, function(req, res){
-    res.render('index.html', {
-        layout: 'layout2',
-        users: req.users
+app.get('/', yz, function(req, res){
+
+    User.find({},{},function(data){
+        res.render('index.html', {
+            users: data
+        });
     });
+
+  
 });
 
-app.get('/open_update_page', function(req, res){
-    if (req.session.adm) {
-        if (req.param('id') && users[req.param('id')]) {
-            res.render('update.html', {
-                updateuser: users[req.param('id')]
-            });
-        }
-        else {
-            res.redirect('/admin');
-        }
-    }
-    else {
-        res.render('login.html');
-    }
+
+// 编辑ID指定User
+app.get('/edit/:id',yz, function(req, res){
+    
+    User.get(req.params.id,function(data){
+        res.render('update.html', {
+           updateuser:data
+        });
+    });    
+    
 });
 
 
