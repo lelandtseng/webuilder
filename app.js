16a58b12aa0388401513bd549d32ed4e70a89022
ubuntu linux <leland@ubuntu.(none)>
2011-02-16 01:44:54 -0800

    at

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var express = require('express');
 
 var app = module.exports = express.createServer(require('connect-form')() , express.staticProvider(__dirname + '/public'), express.bodyDecoder(), express.cookieDecoder(),express.methodOverride(), express.session({ secret: 'keyboard cat' }));
 
-
-//test git push.
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.register('.html', require('ejs'));
